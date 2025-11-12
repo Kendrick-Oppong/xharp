b0ee51a125c00a43e969f512b6c06162d5e23ef9
Kendrick <kendrickoppong94@gmail.com>
2025-11-12 21:59:00 +0000

    refactor: update import path

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import { SideBar } from "./components/side-bar";
+import { SideBar } from "@/components/ui/side-bar";
 
 export default function DashboardLayout({
   children,
@@ -10,10 +10,6 @@ export default function DashboardLayout({
       <SideBar />
 
       <div className="flex-1 flex flex-col overflow-y-auto">
-        {/* <header className="bg-white p-4 shadow-md sticky top-0 z-10">
-          <h2 className="text-lg font-semibold">HOME OVERVIEW</h2>
-        </header> */}
-
         <main className="px-8 mt-[51px] flex-1">{children}</main>
       </div>
     </div>
