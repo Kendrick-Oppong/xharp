7f03e571b5a8d4180fe0ae3e6a95b9737674bdb1
Kendrick <kendrickoppong94@gmail.com>
2025-11-12 19:37:42 +0000

    feat: implement sidebar with navigation, active indicator, caret, and profile section

---
+++ app/dashboard/components/side-bar.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { navItems } from "@/lib/constants/side-nav";
+import { RiArrowRightSLine } from "@remixicon/react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const SideBar = () => {
+  const pathname = usePathname();
+
+  return (
+    <aside className="flex flex-col pl-4 pr-2 pt-6 min-w-[249px] border-r shadow border-r-gray-300">
+      {/* Logo */}
+      <Image
+        src="/xharp.png"
+        width={94}
+        height={35}
+        alt="xharp"
+        className="mb-6 ml-4"
+      />
+
+      {/* Navigation */}
+      <ul className="space-y-3 flex-1">
+        {navItems.map((item, index) => {
+          const isActive = pathname === item.href;
+
+          return (
+            <li
+              key={item.href}
+              className={`text-sm font-medium leading-5 flex items-center ${
+                isActive
+                  ? "bg-primary-foreground/5 font-semibold text-black rounded-lg"
+                  : "text-gray-700"
+              }`}
+            >
+              {isActive && (
+                <div className="w-1 h-5 bg-foreground rounded-r-md -translate-x-3" />
+              )}
+              <Link
+                href={item.href}
+                className="flex items-center justify-between py-2 px-2 w-full"
+              >
+                <span className="inline-flex items-center">
+                  <item.icon className="mr-2" />
+                  {item.name}
+                </span>
+                {index === 0 && <RiArrowRightSLine size={21} />}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+
+      {/* Footer */}
+      <div className="flex items-center gap-3 text-gray-700 pt-5 mb-17 border-t border-t-gray-400">
+        <div className="size-10 bg-gray-300 rounded-full flex items-center justify-center">
+          <Image src="/user.svg" width={40} height={40} alt="user-profile" />
+        </div>
+        <div>
+          <div className="text-sm font-semibold">Nicholas Osei</div>
+          <div className="text-xs text-gray-500 mt-1">nick@gmail.com</div>
+        </div>
+        <div className="ml-5">
+          <RiArrowRightSLine size={21} />{" "}
+        </div>
+      </div>
+    </aside>
+  );
+};
